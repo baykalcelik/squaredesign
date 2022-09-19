6e881db91b84307d4cff4cf04df905c72c0094f6
baykalcelik <baykalcelik2022@gmail.com>
2022-09-19 21:12:12 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'squaredesign'
+import 'squaredesign/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
